41cc621e53d4ffe1ad304bcb80fb406faa4f3355
rufflian <91517002+rufflian@users.noreply.github.com>
2022-07-16 10:59:17 +1000

    ✨ Add convertStringsToPublicKeys util

--- src/instruction/utils.ts
+++ src/instruction/utils.ts
@@ -1,17 +1,51 @@
-import { BN } from "@project-serum/anchor";
+import { web3, BN } from "@project-serum/anchor";
 
-export function convertNumbersToBNs(data: Object | any[], keysToUpdate?: string[]) {
+export function convertNumbersToBNs(
+  data: Object | any[],
+  keysToUpdate: string[] = []
+) {
+  return convertTypeToType(
+    data,
+    keysToUpdate,
+    "number",
+    (value) => new BN(value)
+  );
+}
+
+export function convertStringsToPublicKeys(
+  data: Object | any[],
+  keysToUpdate: string[] = []
+) {
+  return convertTypeToType(
+    data,
+    keysToUpdate,
+    "string",
+    (value) => new web3.PublicKey(value)
+  );
+}
+
+export function convertTypeToType(
+  data: Object | any[],
+  keysToUpdate: string[],
+  existingType: string,
+  toType: (arg0: any) => any
+) {
   if (Array.isArray(data)) {
-    return data.map(item => {
-      if (typeof item == "number") {
-        return new BN(item);
+    return data.map((item) => {
+      if (typeof item == existingType) {
+        return toType(item);
       }
 
       if (keysToUpdate && keysToUpdate.length > 0) {
         let parsedKeys = keysToUpdate[0].split(".");
         const [firstKey, ...remainingKeys] = parsedKeys;
         if (firstKey === "[]" && remainingKeys?.length > 0) {
-          return convertNumbersToBNs(item, [remainingKeys.join(".")]);
+          return convertTypeToType(
+            item,
+            [remainingKeys.join(".")],
+            existingType,
+            toType
+          );
         }
       }
 
@@ -19,21 +53,31 @@ export function convertNumbersToBNs(data: Object | any[], keysToUpdate?: string[
     });
   }
 
-  keysToUpdate && keysToUpdate.forEach((key) => {
-    let parsedKeys = key.split(".");
-    const [firstKey, ...remainingKeys] = parsedKeys;
-    if (parsedKeys.length == 1) {
-      if (Array.isArray(data[firstKey])) {
-        data[firstKey] = convertNumbersToBNs(data[firstKey]);
-      }
+  keysToUpdate &&
+    keysToUpdate.forEach((key) => {
+      let parsedKeys = key.split(".");
+      const [firstKey, ...remainingKeys] = parsedKeys;
+      if (parsedKeys.length == 1) {
+        if (Array.isArray(data[firstKey])) {
+          data[firstKey] = convertTypeToType(
+            data[firstKey],
+            [],
+            existingType,
+            toType
+          );
+        }
 
-      if (typeof data[firstKey] == "number") {
-        data[firstKey] = new BN(data[firstKey]);
+        if (typeof data[firstKey] == existingType) {
+          data[firstKey] = toType(data[firstKey]);
+        }
+      } else {
+        data[firstKey] = convertTypeToType(
+          data[firstKey],
+          [remainingKeys.join(".")],
+          existingType,
+          toType
+        );
       }
-    } else {
-      data[firstKey] = convertNumbersToBNs(data[firstKey], [remainingKeys.join(".")]);
-    }
-  });
-
+    });
   return data;
 }
