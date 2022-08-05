1aabefd46eef0d75b9a9334e7c9a35858a00689b
rufflian <91517002+rufflian@users.noreply.github.com>
2022-08-05 14:20:08 +1000

    ♻️ Use getParsedTokenAccountsByOwner method

--- frameworks/react/src/hooks/useFetchWalletTokens.ts
+++ frameworks/react/src/hooks/useFetchWalletTokens.ts
@@ -5,30 +5,23 @@ import {
   Connection,
   ParsedAccountData,
   PublicKey,
+  RpcResponseAndContext,
 } from '@solana/web3.js';
 import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
 
-type AccountResponse = {
-  pubkey: PublicKey;
-  account: AccountInfo<ParsedAccountData | Buffer>;
-};
+type AccountResponse = RpcResponseAndContext<
+  Array<{
+    pubkey: PublicKey;
+    account: AccountInfo<ParsedAccountData>;
+  }>
+>;
 
 const getAccounts = (
   connection: Connection,
   publicKey: PublicKey,
-): Promise<AccountResponse[]> =>
-  connection.getParsedProgramAccounts(TOKEN_PROGRAM_ID, {
-    filters: [
-      {
-        dataSize: 165, // number of bytes
-      },
-      {
-        memcmp: {
-          offset: 32, // number of bytes
-          bytes: publicKey.toString(), // base58 encoded string
-        },
-      },
-    ],
+): Promise<AccountResponse> =>
+  connection.getParsedTokenAccountsByOwner(publicKey, {
+    programId: TOKEN_PROGRAM_ID,
   });
 
 const fetchTokenInfoFromAccounts = async (
@@ -40,15 +33,10 @@ const fetchTokenInfoFromAccounts = async (
   callback(collateTokenInfoFromAccounts(accounts));
 };
 
-const collateTokenInfoFromAccounts = (
-  accounts: AccountResponse[],
-): TokenInfo[] =>
-  accounts
-    .filter(({ account: { data } }) => 'parsed' in data)
+const collateTokenInfoFromAccounts = (accounts: AccountResponse): TokenInfo[] =>
+  accounts.value
     .map(({ account: { data } }) => ({
-      // @ts-ignore
       mintAddr: data?.parsed?.info?.mint,
-      // @ts-ignore
       qty: data?.parsed?.info?.tokenAmount?.uiAmount,
     }))
     .filter(({ mintAddr, qty }) => !!(mintAddr && qty));
